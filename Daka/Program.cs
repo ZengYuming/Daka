84373cda6da1727286f6bc40a0844a31b6aab631
ZengYuming <605975553@qq.com>
2015-07-29 03:01:01 -0700

    vs2012 solution code
    vs2012 solution code

---
+++ Daka/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Daka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Note: If you get exception of IE security issue then you can look at http://stackoverflow.com/questions/14952348/not-able-to-launch-ie-browser-using-selenium2-webdriver-with-java
+            SeleniumHelper.Execute();
+
+            Console.Read();
+        }
+
+
+    }
+}
